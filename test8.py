1264cd83dc3e7982a850e79f9f924732c2c35c7c
Roy SALIBA <134060194+Royalcoco@users.noreply.github.com>
2023-12-01 17:03:21 +0100

    Add files via upload

---
+++ test8.py
@@ -0,0 +1,35 @@
+import sys from @next :;//<form action="{{url('admin/users')}}" method="post">
+                                                                                            <script src="{}/static/js/jquery-3.2.1.slim.min.js"></script>
+                                                                                            <script src="/static/js/jquery-3.2.1.slim.min.js"></script>
+                                                                                                                                    <script src="{}/static/js/jquery-3.5.1.slim.min.js"></script>
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    
+                                                                                                                                    import jquery from 'jquery
+                                                                                                                                    @csrf
+                                                                                                                                    @case *args, **kwargs. /super.//__loader__
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+                                                                                                                                    @csrf
+
+                                                                                                                                    format = 'size by height'(except, KeyboardInterrupt )
+                                                                                                                                    <div class="row">
+                                                                                                                                    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+                                                                                                                                    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+                                                                                                                                    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+                                                                                                                                    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+                                                                                                                                    <div class="container-fluid">
+                                                                                                                                    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+
+                                                                                                                                    
