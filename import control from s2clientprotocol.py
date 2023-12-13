162281877e647271e8a7e864caee52e31f9a5627
Roy SALIBA <134060194+Royalcoco@users.noreply.github.com>
2023-12-13 11:27:16 +0100

    Add files via upload

--- import control from s2clientprotocol.py
+++ import control from s2clientprotocol.py
@@ -212,3 +212,226 @@ Parameters:
                 @yield('post-content')
                 
                 
+                
+                </div>
+                @stop
+                @section('footer')
+                
+                <div id="footer">
+                <div class="container text-center">
+                <p>Copyright &copy; {{ date("Y") }} <a href="/">Laravel Blog</a>. Theme by
+                <a href="http://startbootstrap.com/template-overviews/sb-admin-2">SB Admin 2</a>,
+                
+                designed and customized by <a href="https://github.com/jbrooksuk">James Brookes</a>.</p
+                </div>
+                
+                
+                </div>
+                @stop
+                @section('scripts')
+                <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></
+                script>
+                <script type="text/javascript" src="{{ asset('/assets/js/metisMenu.js')
+                }}"></script>
+                <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"><
+                /script>
+                <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.
+                js"></script>
+                <script src="{{asset ('assets/js/morris.js')}}"></script>
+                
+                <script src="//cdn.ckeditor.com/4.4.6/standard/ckeditor.js"></script>
+                <script>CKEDITOR.replace('body');</script>
+                
+                <script src="{{ asset('/assets/js/custom.js') }}"></script>
+                <script>
+                $(document).ready(function() {
+                    $('#side-menu').metisMenu();
+                    });
+                    </script>
+                    @if (Session::has('success'))
+                    <script>
+                    swal({title: "Success!", text: "{{ Session::get('success') }}", type: "success"}, function(){
+                        swal({title:"Success!",text:"{{ Session::get('success') }}",type:"success"},
+                            function(){location.reload();});
+                        </script>
+                        
+                        @endif
+                        @if ($errors->any())
+                        
+                        
+                        <script>swal({title:"Error!",text:"Please check the form for errors.",type: "error"});</script
+                        <!--   Only show error notification if there is an actual error to display -->
+                        <script>swal({title: "Error!",text: "@foreach($errors->all() as $error)<li>
+                                    {{ $error }}</li>@endforeach",type: "error"});</script>
+                        @endif
+                        </script>
+                        </html>
+                        </div></s>
+                        #include "stdafx.h"
+                        
+                        #include "CppUnitTest.h"
+                        using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+                        TEST_CLASS(MyTests)
+                        {
+                            public:
+                                /*
+                                * Testing the addition of two integers
+                                */
+                                TEST_METHOD(AdditionOfTwoIntegers)
+                                {
+                                    int a = 5, b = 8;
+                                    Assert::AreEqual(13, a + b);
+                                    }
+                                };
+                        </s>
+                        #include "gtest/gtest.h"
+                        #include "../src/utilities/StringManipulation.hpp"
+                        // The fixture for testing class Foo.
+                        
+                        class StringManipulationTest : public ::testing::Test
+                        }
+                        protected:
+                             // If the constructor and destructor are not enough for setting up
+                             // and tearing down each test, you can define the following methods:
+                                virtual void SetUp()
+                                {
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test)
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // Code here will be called immediately after the constructor (right before each test).
+                                     // _ensure_connected
+                                    }
+                                virtual void TearDown()
+                                {
+                                     // _disconnect
+                                    }
+                                };
+                    /**
+                    * @brief Checks if string is empty or not
+                    */
+                    TEST_F(StringManipulationTest, IsEmpty)
+                    {
+                        std::string str("");
+                        ASSERT_TRUE(is_empty(str));
+                        }
+                    /**
+                    * @brief Checks if string is not empty
+                    */
+                    
+                    TEST_F (StringManipulationTest, NotIsEmpty)
+                    {
+                        std::string str("Hello World!");
+                        ASSERT_FALSE(is_empty(str));
+                        }
+                    /**
+                    * @brief Appends two strings
+                    */
+                    TEST_F(StringManipulationTest, AppendStrings)
+                    
+                    
+                    {
+                        std::string s1 = "Hello";
+                        std::string s2 = "World!";
+                        append_strings(s1, s2);
+                        ASSERT_EQ(s1, "HelloWorld!");
+                        s1 = "1234567890";
+                        append_strings(s1, s2);.Bring.sum(self, *args, **kwargs):
+                                return super().bring
+                                .sum(*args, **kwargs) + len(self._name)</s>
+                                #include <stdio.h>
+                                #include "gtest/gtest.h"
+                                class MyTestFixture : public ::testing::Test {
+                                    
+                                    protected:
+                                        MyTestFixture() {}
+                                        ~MyTestFixture() {}
+                                        
+                                        void SetUp() override {
+                                            printf("SetUp\n");
+                                            }
+                                        void TearDown() override {
+                                            printf("TearDown\n");
+                                            }
+                                        };
+                                // Test Fixture for testing add function
+                                
+                                TEST_F(MyTestFixture, AddFunction) {
+                                    int a = 1;
+                                    int b = 2;
+                                    
+                                    EXPECT_EQ(a+b, 3);
+                                    }</s>}</s>}
+                    }
+                    } else {
+                        /* No match found in the table - use default action */
+                        rv = BCMFP_FILTER_SELECTOR_ERROR;
+                        }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    
+                    }
+                    
+                    
+                    }
+                    }
+                    if (BCMFP_ACTIONS_ATTRS_CHECK(unit)) {
+                        
+                        /* Check that all attributes are set correctly. */
+                        rv = bcmfp_actions_attrs_check(unit, stage,
+                                                    &grp_oper->action_res_id,
+                                                    &grp_oper->action_res_chunk_id,
+                                                    grp_oper->num_actions,
+                                                    fg->flags);
+                        }
+                    }
+                    }
+                    }
+                    }
+                    }
+                    break;
+                case BCMFP_GROUP_MODE_PORT:
+                    case BCMFP_GROUP_MODE_LPORT:
+                        case BCMFP_GROUP_MODE_HPORT:
+                            key_size = 0;
+                            data_size = 0;
+                            num_qos_prof = 0;
+                            
+                            num_ltpr_prof = 1;
+                            num_tcAMs = 1;
+                            num_packet_format_types = 1;
+                            num_vlan_format_types = 1;
+                            num_ip_protocol_types = 1;
+
+                            
+                            num_tcp_ctl_bits = 8;
+                            num_udp_ctl_bits = 8;
+                            num_icmp_type_groups = 4;
+                            num_mpls_specs = 3;
+                            num_vxlan_vpn = 2;
+                            is_presel = FALSE;
+                            id_alloc_one = TRUE;
+                            action_res_id_valid = TRUE;
+                            action_res_level = BCMFP_HA_BLK_ENTRY_ID;
+                            blk_id = -1;
+                            chunk_id = -1;
+                            
